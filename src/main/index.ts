fb21763a6b7f7956fca5e35ea12b0bd0423b19da
thalles <thallesgabriel1307@gmail.com>
2022-06-30 20:34:59 -0300

    chore: use streams

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,9 +2,8 @@ import http from 'http'
 import fs from 'fs'
 
 const server = http.createServer((req, res) => {
-  fs.readFile(`${__dirname}/../../requirements/CNAB.txt`, (err, data) => {
-    res.end(data)
-  })
+  const stream = fs.createReadStream(`${__dirname}/../../requirements/CNAB.txt`)
+  stream.pipe(res)
 })
 
 server.listen(5858)
