8a77064da5272b3b50b88920ded49d6b15455df1
thalles <thallesgabriel1307@gmail.com>
2022-06-30 20:47:42 -0300

    chore: configure server

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,11 @@
 import http from 'http'
 import fs from 'fs'
 
-const server = http.createServer((req, res) => {
-  const stream = fs.createReadStream(`${__dirname}/../../requirements/CNAB.txt`)
-  stream.pipe(res)
-})
+const PORT = process.env.PORT || 5858
 
-server.listen(5858)
+export const server = http
+  .createServer((req, res) => {
+    const stream = fs.createReadStream(`${__dirname}/../../requirements/CNAB.txt`)
+    stream.pipe(res)
+  })
+  .listen(PORT, () => console.log(`Server is running at ${PORT}`))
