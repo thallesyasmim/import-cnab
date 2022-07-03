3f212c9f249f569ff671cb4808f1e5e1a21d1cd5
thalles <thallesgabriel1307@gmail.com>
2022-07-03 16:05:47 -0300

    chore: register typeorm repositories in container

--- src/infra/database/typeorm/repositories/index.ts
+++ src/infra/database/typeorm/repositories/index.ts
@@ -1,2 +1,19 @@
+import {
+  LoadShopsRepository,
+  SaveShopRepository,
+  SaveTransactionRepository
+} from '@/application/contracts'
+import { ShopTypeormRepository } from './shop-repository'
+import { TransactionTypeormRepository } from './transaction-repository'
+import { container } from 'tsyringe'
+
+container.register<LoadShopsRepository & SaveShopRepository>('ShopTypeormRepository', {
+  useClass: ShopTypeormRepository
+})
+
+container.register<SaveTransactionRepository>('TransactionTypeormRepository', {
+  useClass: TransactionTypeormRepository
+})
+
 export * from './shop-repository'
 export * from './transaction-repository'
