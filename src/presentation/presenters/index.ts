bfcd3ad33cf16e754e84327b318ef3466a0f3d13
thalles <thallesgabriel1307@gmail.com>
2022-07-03 17:50:24 -0300

    chore: register UploadCnabFilePresenter

--- src/presentation/presenters/index.ts
+++ src/presentation/presenters/index.ts
@@ -1,9 +1,15 @@
 import { container } from 'tsyringe'
 import { Gateway } from '../protocols'
 import { LoadShopsPresenter } from './load-shops-presenter'
+import { UploadCnabFilePresenter } from './upload-cnab-file'
 
 container.register<Gateway>('LoadShopsPresenter', {
   useClass: LoadShopsPresenter
 })
 
+container.register<Gateway>('UploadCnabFilePresenter', {
+  useClass: UploadCnabFilePresenter
+})
+
 export * from './load-shops-presenter'
+export * from './upload-cnab-file'
