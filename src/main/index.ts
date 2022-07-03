82669a29ae731445a0ec572154c71c23975ed868
thalles <thallesgabriel1307@gmail.com>
2022-07-03 15:17:09 -0300

    chore: use Writable stream

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,19 @@
 import http from 'http'
 import fs from 'fs'
+import { Stream } from 'stream'
 
 const PORT = process.env.PORT || 5858
 
 export const server = http
   .createServer((req, res) => {
     const stream = fs.createReadStream(`${__dirname}/../../requirements/CNAB.txt`)
-    stream.pipe(res)
+
+    const writableStream = new Stream.Writable()
+    writableStream._write = (chunk, enconding, next) => {
+      console.log(chunk.toString())
+      next()
+    }
+
+    stream.pipe(writableStream)
   })
   .listen(PORT, () => console.log(`Server is running at ${PORT}`))
