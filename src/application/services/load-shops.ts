dfc105b2407ef7769f4412aa03929d2b862a3e51
thalles <thallesgabriel1307@gmail.com>
2022-07-03 16:04:16 -0300

    chore(service): add LoadShopsService

--- src/application/services/load-shops.ts
+++ src/application/services/load-shops.ts
@@ -1,10 +1,15 @@
 import { Shop } from '@/domain/models'
 import { LoadShops } from '@/domain/usecases'
 import { Either, right, left } from '@/shared/either'
+import { inject, injectable } from 'tsyringe'
 import { LoadShopsRepository } from '../contracts'
 
+@injectable()
 export class LoadShopsService implements LoadShops {
-  constructor(private readonly loadShopsRepository: LoadShopsRepository) {}
+  constructor(
+    @inject('ShopTypeormRepository')
+    private readonly loadShopsRepository: LoadShopsRepository
+  ) {}
 
   async load(): Promise<Either<null, Shop[]>> {
     const shops = await this.loadShopsRepository.load()
