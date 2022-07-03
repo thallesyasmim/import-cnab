d81e6fe5000861b381eb4ba9ace1f63db0b9dbad
thalles <thallesgabriel1307@gmail.com>
2022-07-03 17:48:30 -0300

    chore(route): add shop routes

---
+++ src/main/routes/shop-routes.ts
@@ -0,0 +1,10 @@
+import { Gateway } from '@/presentation/protocols'
+import { adaptRoute } from '../adapters'
+
+import { Router } from 'express'
+import { container } from 'tsyringe'
+
+export default (router: Router): void => {
+  const loadShopsPresenter: Gateway = container.resolve('LoadShopsPresenter')
+  router.get('/shops', adaptRoute(loadShopsPresenter))
+}
