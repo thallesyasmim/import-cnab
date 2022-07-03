cb07bebb8bafda0374838ce86ea19d90792d6b1e
thalles <thallesgabriel1307@gmail.com>
2022-07-03 15:37:15 -0300

    chore: add express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,19 @@
+import { Gateway } from '@/presentation/protocols'
+import { Request, Response } from 'express'
+
+export const adaptRoute = (controller: Gateway) => {
+  return async (req: Request, res: Response) => {
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {})
+    }
+
+    const httpResponse = await controller.handle(request)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299)
+      return res.status(httpResponse.statusCode).json(httpResponse.body)
+
+    return res.status(httpResponse.statusCode).json({
+      error: httpResponse.body?.message
+    })
+  }
+}
